75511d02873a4865f6bc88469448e7ce00c6ed4b
hejianliang <844131681@qq.com>
2021-02-20 17:17:28 +0800

    添加子、父组件

---
+++ src/components/views/first.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="first-app">
+        {{msg}}
+        <confirm text="注册" @message="getMessage"></confirm>
+    </div>
+</template>
+
+<script>
+import Confirm from '../sub/Confirm'
+export default {
+    name: 'first',
+    data() {
+        return {
+            msg: 'welcome to first app'
+        };
+    },
+    components: {
+        Confirm
+    },
+    methods: {
+        getMessage(val) {
+            alert(val);
+        }
+    }
+}
+</script>
