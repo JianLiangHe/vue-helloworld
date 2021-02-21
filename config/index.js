062cea2b2d0e5f255ded23f8f72cb7ac115406d8
hejianliang <844131681@qq.com>
2021-02-21 10:28:39 +0800

    修改端口，设置启动自动打开浏览器

--- config/index.js
+++ config/index.js
@@ -14,8 +14,9 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    port: 8088, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    //autoOpenBrowser: false, // 启动是否自动打开浏览器
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
